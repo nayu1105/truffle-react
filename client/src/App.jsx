c8462c8590c38f202c239c2e8ae73509f30a8044
nayu99 <nyj3230@gmail.com>
2023-03-17 18:01:41 +0900

    erc7212 nft contract 작성 중

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Web3 from "web3";
+// import Web3 from "web3";
 
 function App() {
+  const [web3, setWeb3] = useState();
   const navigate = useNavigate();
   const [account, setAccount] = useState(); // state variable to set account.
 
@@ -17,20 +18,43 @@ function App() {
   //   load();
   // }, []);
 
-  async function load() {
-    const web3 = new Web3(Web3.givenProvider);
-    // const web3 = new Web3("http://localhost:7545");
-    const accounts = await web3.eth.requestAccounts();
-    if (!window.ethereum) {
-      // console.error("MetaMask is not found!");
-      navigate("https://metamask.io/");
+  // async function load() {
+  //   const web3 = new Web3(Web3.givenProvider);
+  //   // const web3 = new Web3("http://localhost:7545");
+  //   const accounts = await web3.eth.requestAccounts();
+  //   if (!window.ethereum) {
+  //     // console.error("MetaMask is not found!");
+  //     navigate("https://metamask.io/");
+  //   } else {
+  //     console.log("설치됨");
+  //   }
+  //   // 첫번째 계정을 accounts 로 설정
+  //   setAccount(accounts);
+  //   // console.log(typeof accounts);
+  // }
+
+  const load = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        const web = new Web3(window.ethereum);
+        setWeb3(web);
+      } catch (err) {
+        console.log(err);
+      }
     } else {
-      console.log("설치됨");
+      navigate("https://metamask.io/");
     }
-    // 첫번째 계정을 accounts 로 설정
-    setAccount(accounts);
-    // console.log(typeof accounts);
-  }
+  };
+
+  //  const accounts = await window.ethereum.request({
+  //   method: "eth_requestAccounts",
+  // });
+  const connectWallet = async () => {
+    accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+  };
 
   return (
     <>
---
+++ truffle/contracts/NFT.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol"; //
+
+
+contract NFT is ERC721URIStorage, Ownable{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    // 토큰 id 발행에 쓰이는 Counter
+    // current(counter)를 하면 uint256 이 나옴 => tokenId
+    // 이를 increment(counter) 하면서 tokenId 증가하면서 계속 발행해주면 됨.
+
+    constructor() public ERC721("NFT", "NFT"){
+
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        // memory & storage
+        // Storage는 블록체인 상에 영구적으로 저장되며, 
+        // Memory는 임시적으로 저장되는 변수로 함수의 외부 호출이 일어날 때마다 초기화
+
+        // 비유하자면 Storage 는 하드디스크, Memory는 RAM에 저장하는 것.
+        return super.tokenURI(tokenId);
+    }
+
+    function mint()
+        public
+        onlyOwner
+        returns(uint256)    
+    {   
+        _tokenIds.increment();  
+        return newNFTId;
+    }
+
+    function _burn(unit256 tokenId)
+        internal 
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+}
